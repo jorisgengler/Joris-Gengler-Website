f5b52fa5bbfe3a2ee2483cee1ea0f1a98742481f
jorisgengler <jorisgengler@hotmail.com>
2017-10-30 16:38:13 +0100

    maps

--- java.js
+++ java.js
@@ -1,5 +1,5 @@
 
-//scroll//
+//---------------------scroll---------------------//
 
 $(document).ready(function(){
   
@@ -24,6 +24,29 @@ $(document).ready(function(){
     } 
   });
 });
+
+
+//---------------------Contact Alert---------------------//
+function contactalert() {
+    alert("Oeps werkt nog niet");
+}
+
+//---------------------google maps AIzaSyDt9QutH8JlD-GQ-7GHXUAFXFGruWNu3IA ---------------------//
+
+function myMap() {
+  var myCenter = new google.maps.LatLng(52.71369850000001,6.887724599999956);
+  var mapCanvas = document.getElementById("googleMap");
+  var mapOptions = {center: myCenter, zoom: 15};
+  var map = new google.maps.Map(mapCanvas, mapOptions);
+  var marker = new google.maps.Marker({position:myCenter});
+  var infowindow = new google.maps.InfoWindow({content: "Joris Gengler"});
+  
+ 
+	marker.setMap(map);
+	infowindow.open(map,marker);
+
+
+}
 
 
 
